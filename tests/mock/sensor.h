9362fde1b901f54b762fa86ffdee2ea703b390f0
bene <benedikt.wiethof@rwth-aachen.de>
2023-05-25 20:56:57 +0200

    test: Add tests for sensor implementations

---
+++ tests/mock/sensor.h
@@ -0,0 +1,56 @@
+//
+// Created by bene on 02.06.23.
+//
+
+
+#pragma once
+
+#include "base/base_sensor.hpp"
+#include "slam/sensor.hpp"
+#include "core/helper.hpp"
+
+namespace ukf::test::mock {
+    struct Data {
+        struct {
+            float a;
+            float b;
+            float c;
+            float d;
+        } measure;
+        struct {
+            float a[4];
+            float b[4];
+            float c[4];
+            float d[4];
+        } noising;
+    };
+
+    const ukf::test::mock::Data values[] = {
+            {
+                    {1, 2, 3, 4},
+                    {{1},                      {0,    2}, {0,    0,    3},          {0,    0,    0,    4}}
+            },
+            {
+                    {1, 2, 3, 4},
+                    {{1.1f, 1.2f, 1.3f, 1.4f}, {2.1f, 2.2f, 2.2f, 2.3f},
+                                                          {3.1f, 3.2f, 3.3f, 3.4f}, {4.1f, 4.2f, 4.3f, 4.4f}}
+            }
+    };
+
+
+    class BaseSensor : public ukf::StaticSensor<4, Data> {
+    public:
+        [[nodiscard]] DataContainerType z(const Data &data) const override;
+
+        [[nodiscard]] NoiseContainerType R(const Base::Type &data) const override;
+    };
+
+    class SlamSensor : public ukf::slam::Sensor<4, Data> {
+    public:
+        using ukf::slam::Sensor<4, Data>::Sensor;
+
+        [[nodiscard]] DataContainerType z(const Type &data) const override;
+
+        [[nodiscard]] NoiseContainerType R(const Type &data) const override;
+    };
+} // ukf::test::mock
---
+++ tests/slam/sensor_test.cpp
@@ -0,0 +1,42 @@
+//
+// Created by bene on 25.05.23.
+//
+
+#include <gtest/gtest.h>
+#include "base/base_sensor.hpp"
+#include "mock/sensor.h"
+
+namespace ukf::test::slam {
+
+    class SlamSensorFixture : public testing::TestWithParam<ukf::test::mock::Data> {
+    };
+
+    TEST_P(SlamSensorFixture, SensorImpl) {
+        ukf::test::mock::Data data = GetParam();
+        const Eigen::Vector4f expectedMeasurement = {data.measure.a, data.measure.b, data.measure.c, data.measure.d};
+        Eigen::Matrix4f expectedNoising;
+        expectedNoising << data.noising.a[0], data.noising.a[1], data.noising.a[2], data.noising.a[3],
+                data.noising.b[0], data.noising.b[1], data.noising.b[2], data.noising.b[3],
+                data.noising.c[0], data.noising.c[1], data.noising.c[2], data.noising.c[3],
+                data.noising.d[0], data.noising.d[1], data.noising.d[2], data.noising.d[3];
+
+        ukf::test::mock::SlamSensor sensor(data, 0);
+        EXPECT_EQ(sensor._z(), expectedMeasurement);
+        EXPECT_EQ(sensor._R(), expectedNoising);
+    };
+
+
+    INSTANTIATE_TEST_SUITE_P(BaseSensor, SlamSensorFixture, testing::ValuesIn(ukf::test::mock::values));
+
+
+// NoOp Sensor shall do no Operation
+    TEST(sensor, NoOp) {
+        ukf::NoOpSensor s;
+
+        const Eigen::Vector<float, 0> expectedMeasurement;
+        const Eigen::DiagonalMatrix<float, 0> expectedNoising;
+
+        EXPECT_EQ(s._z({}), expectedMeasurement);
+        EXPECT_EQ(s._R({}), expectedNoising.toDenseMatrix());
+    }
+}

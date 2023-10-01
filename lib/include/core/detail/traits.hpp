9c657654f8dd22e940d8f8971fa524e051b7e1aa
bene <benedikt.wiethof@rwth-aachen.de>
2023-10-01 16:33:48 +0200

    feat: Add basic implementation for SRUKF
    Add math operations needed by the SRUKF implementation
    The Ukf implements the standard steps performed by the SRUKF.
    It is independent of the usage in context of State estimation only or
    SLAM.
    Further implementations need to provide the System noising since it
    might be variable in the context of SLAM.
    
    Signed-off-by: bene <benedikt.wiethof@rwth-aachen.de>

---
+++ lib/include/core/detail/traits.hpp
@@ -0,0 +1,18 @@
+//
+// Created by bene on 13.05.23.
+//
+#pragma once
+
+#include <cstddef>
+
+namespace ukf {
+    namespace core {
+        namespace detail {
+            template<typename ...Args>
+            struct Pack {
+            };
+
+            template<typename T> constexpr std::size_t FieldSize = T::Size;
+        };
+    };
+};
